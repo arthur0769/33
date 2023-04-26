5dccd5dbe8de45c81c71287a51de7fef81ba20d5
Arthur <arthurhlsouza@gmail.com>
2023-04-25 21:23:21 -0300

    update

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Firestore, collectionData, addDoc } from "@angular/fire/firestore";
 import { ProviderId } from "firebase/auth";
 import { collection } from "firebase/firestore";
+import { query, where } from 'firebase/firestore';
 
 export interface Cards {
     id?: string;
@@ -21,6 +22,14 @@ export class DataService {
         const cardsRef = collection(this.firestore, 'cards');
         return collectionData(cardsRef);
     }
+
+    getCardsHoje() {
+        const cardsRef = collection(this.firestore, 'cards');
+        const hoje = new Date();
+        const data = `${hoje.getDate()}-${hoje.getMonth() + 1}-${hoje.getFullYear()}`;
+        const q = query(cardsRef, where('data', '==', data));
+        return collectionData(q);
+      }
 
     addCards(cards: Cards) {
       const cardsRef = collection(this.firestore, 'cards');
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,14 +6,35 @@ import { DataService } from '../services/data.service';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
+
 export class Tab2Page {
 
+  public cardsEstudar: any[]
+  public currentIndex = 0;
+  
+
   constructor(private dataService: DataService) {
-    this.dataService.getCards().subscribe(res => {
-      console.log(res);
-    }
-   )
+
+    this.cardsEstudar = [];
+    
+
+    this.dataService.getCardsHoje().subscribe(res => {
+      this.cardsEstudar = res;
+      console.log(this.cardsEstudar);
+    })
   }
+
+  public get currentCard() {
+    return this.cardsEstudar[this.currentIndex];
+  }
+
+  public nextCard() {
+    this.currentIndex = (this.currentIndex + 1) % this.cardsEstudar.length;
+  }
+
+  
+  
 
 }
   
