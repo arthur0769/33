c10cfe09f9bc350ee64ad91fc2821b0906e91d4a
Arthur <arthurhlsouza@gmail.com>
2023-04-28 20:38:59 -0300

    update

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@angular/core";
-import { Firestore, collectionData, addDoc } from "@angular/fire/firestore";
+import { Firestore, collectionData, addDoc, setDoc, doc } from "@angular/fire/firestore";
 import { ProviderId } from "firebase/auth";
-import { collection } from "firebase/firestore";
+import { and, collection } from "firebase/firestore";
 import { query, where } from 'firebase/firestore';
 
 export interface Cards {
     id?: string;
     pergunta: string
     resposta: string
+    data: string
 }
 
 @Injectable({
@@ -29,11 +30,24 @@ export class DataService {
         const data = `${hoje.getDate()}-${hoje.getMonth() + 1}-${hoje.getFullYear()}`;
         const q = query(cardsRef, where('data', '==', data));
         return collectionData(q);
-      }
+    }
 
     addCards(cards: Cards) {
-      const cardsRef = collection(this.firestore, 'cards');
-      return addDoc(cardsRef, cards);
+        const cardsRef = collection(this.firestore, 'cards');
+        return addDoc(cardsRef, cards);
     }
+
+    atualizarCard(id: string, novasInformacoes: Cards) {
+        if (!novasInformacoes || !novasInformacoes.pergunta) {
+          console.error('As novas informações estão vazias ou nulas!');
+          return null;
+        }
+        const cardsRef = doc(this.firestore, 'cards', id);
+        return setDoc(cardsRef, novasInformacoes, { merge: true });
+      }
+      
+      
+      
+
 
 }
