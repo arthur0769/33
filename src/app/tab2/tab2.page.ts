ca43e062bc7e5ae96b15dc905ee2f5351fa8eff2
Arthur <arthurhenrique0769@gmail.com>
2023-05-31 13:04:14 -0300

    update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -11,6 +11,13 @@ export class Tab2Page {
   public cardsEstudar: any[] = [];
   public currentIndex = 0;
   
+  startX: number = 0; // starting point of the swipe
+  endX: number = 0; //ending point of the swipe
+  startY: number = 0; // starting point of the swipe on Y axis
+  endY: number = 0; // ending point of the swipe on Y axis
+  
+
+
   constructor(private dataService: DataService) {}
 
   private refreshCards() {
@@ -30,6 +37,116 @@ export class Tab2Page {
     return this.cardsEstudar[this.currentIndex];
   }
   
+  touchStart(evt: any) {
+    this.startX = evt.touches[0].pageX;
+    this.startY = evt.touches[0].pageY;
+  }
+
+  touchMove(evt: any, index: number) {
+    let deltaX = this.startX - evt.touches[0].pageX;
+    let deltaY = this.startY - evt.touches[0].pageY;
+    let deg = deltaX /10
+    this.endX = evt.touches[0].pageX;
+    this.endY = evt.touches[0].pageY;
+
+    //Swipe gesture
+
+    (<HTMLStyleElement>document.getElementById("card-" + index)).style.transform = `translate(${ -deltaX }px, ${ -deltaY }px) rotate(${ -deg }deg)`;
+
+
+    if ((this.endX - this.startX) < 0) { // show dislike icon
+      (<HTMLStyleElement>document.getElementById("reject-icon")).style.opacity = String(deltaX / 100);
+    }
+    else {
+      (<HTMLStyleElement>document.getElementById("accept-icon")).style.opacity = String(-deltaX / 100);
+    }
+  }
+
+  touchEnd(index: number) {
+    if (this.endX > 0) { //to avoid removing card on click
+      let finalX = this.endX - this.startX;
+
+      if (finalX > -100 && finalX < 100) { // reset card back to position
+        (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = ".3s";
+        (<HTMLStyleElement>document.getElementById("card-" + index)).style.transform = "translateX(0px) rotate(0deg)";
+
+        // remove transition after 350 ms
+
+        setTimeout(() => {
+          (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = "0s";
+        }, 350);
+      }
+      else if (finalX <= -100) {  //Arrasta pra direita
+        (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = "1s";
+        (<HTMLStyleElement>document.getElementById("card-" + index)).style.transform = "translateX(-1000px) rotate(-30deg)";
+        this.adicionar1Dia()
+
+        //remove user from users array
+
+        setTimeout(() => {
+          this.cardsEstudar.splice(index, 1);
+        }, 100);
+      }
+      else if (finalX >= 100) { //Arrasta pra esquerda
+        (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = "1s";
+        (<HTMLStyleElement>document.getElementById("card-" + index)).style.transform = "translateX(1000px) rotate(30deg)";
+        this.adicionar3Dias()
+
+        //remove user from users array
+
+        setTimeout(() => {
+          this.cardsEstudar.splice(index, 1);
+        }, 100);
+      }
+
+
+      if (this.endY > 0) { //to avoid removing card on click
+        let finalY = this.endY - this.startY;
+  
+        if (finalY > -100 && finalY < 100) { // reset card back to position
+          (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = ".3s";
+          (<HTMLStyleElement>document.getElementById("card-" + index)).style.transform = "translateX(0px) rotate(0deg)";
+  
+          // remove transition after 350 ms
+  
+          setTimeout(() => {
+            (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = "0s";
+          }, 350);
+        }
+        else if (finalY <= -100) {  //Arrasta pra cima
+          (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = "1s";
+          (<HTMLStyleElement>document.getElementById("card-" + index)).style.transform = "translateY(-1000px) rotate(-30deg)";
+          this.adicionar5Dias()
+  
+          //remove user from users array
+  
+          setTimeout(() => {
+            this.cardsEstudar.splice(index, 1);
+          }, 100);
+        }
+        else if (finalY >= 100) { //Arrasta pra baixo
+          (<HTMLStyleElement>document.getElementById("card-" + index)).style.transition = "1s";
+          (<HTMLStyleElement>document.getElementById("card-" + index)).style.transform = "translateY(1000px) rotate(30deg)";
+  
+          //remove user from users array
+  
+          setTimeout(() => {
+            this.cardsEstudar.splice(index, 1);
+          }, 100);
+        }}
+
+      //reset all
+
+      this.startX = 0;
+      this.endX = 0;
+      this.startY = 0; 
+      this.endY = 0;
+      (<HTMLStyleElement>document.getElementById("reject-icon")).style.opacity = "0";
+      (<HTMLStyleElement>document.getElementById("accept-icon")).style.opacity = "0";
+    }
+  }
+
+
     adicionar5Dias() {
       const currentCard = this.cardsEstudar[this.currentIndex];
       if (currentCard) {
